e6bec1efd5205abdaaec5097aae2af857f8709f6
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-11 15:46:55 +0530

    worked

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ import vendor from './routes/Vendor.js';
 const myapp = express();
 myapp.use(express.json());
 myapp.use(cors());
-myapp.use('/api/v1/vendors', vendor )
+// myapp.use('/api/v1/vendors', vendor )
 
-await mongoose.connect(process.env.MongoURI).then(()=>{
-    console.log('DataBase Connected');
-}).catch((error)=>{console.log(error)});
+// await mongoose.connect(process.env.MongoURI).then(()=>{
+//     console.log('DataBase Connected');
+// }).catch((error)=>{console.log(error)});
 
 
 myapp.get("/",(req,res)=>{
