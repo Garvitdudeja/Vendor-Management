8e116d04490a31527971e7083eabdce3a17ba3ff
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-11 16:12:39 +0530

    worked

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,17 @@ import vendor from './routes/Vendor.js';
 const myapp = express();
 myapp.use(express.json());
 myapp.use(cors());
-myapp.use('/api/v1/vendors', vendor )
+// myapp.use('/api/v1/vendors', vendor )
 
 
-  await mongoose
-    .connect("mongodb+srv://dudejagarvit3:Garvit1839@cluster0.aa2bbme.mongodb.net/")
-    .then(() => {
-      console.log("DataBase Connected");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+//   await mongoose
+//     .connect("mongodb+srv://dudejagarvit3:Garvit1839@cluster0.aa2bbme.mongodb.net/")
+//     .then(() => {
+//       console.log("DataBase Connected");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
 
 
 myapp.get("/", (req, res) => {
