284d4737f59c269273c5036fbede020cdf9edae3
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-11 15:50:48 +0530

    worked

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 // import dotenv from 'dotenv'
 // dotenv.config()
 import express from "express";
-// import cors from 'cors';
+import cors from 'cors';
 // import mongoose from "mongoose";
 // import vendor from './routes/Vendor.js';
 
 const myapp = express();
 myapp.use(express.json());
-// myapp.use(cors());
+myapp.use(cors());
 // myapp.use('/api/v1/vendors', vendor )
 
 // await mongoose.connect(process.env.MongoURI).then(()=>{
