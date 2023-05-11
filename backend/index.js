718f1f07e50bb259e1ff7d2410ca05bd3ae53e7d
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-11 15:39:00 +0530

    worked

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,35 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from "express";
-import cors from 'cors';
-import mongoose from "mongoose";
-import vendor from './routes/Vendor.js';
+// import dotenv from 'dotenv'
+// dotenv.config()
+// import express from "express";
+// import cors from 'cors';
+// import mongoose from "mongoose";
+// import vendor from './routes/Vendor.js';
 
-const myapp = express();
-myapp.use(express.json());
-myapp.use(cors());
-myapp.use('/api/v1/vendors', vendor )
+// const myapp = express();
+// myapp.use(express.json());
+// myapp.use(cors());
+// myapp.use('/api/v1/vendors', vendor )
 
-await mongoose.connect(process.env.MongoURI).then(()=>{
-    console.log('DataBase Connected');
-}).catch((error)=>{console.log(error)});
+// await mongoose.connect(process.env.MongoURI).then(()=>{
+//     console.log('DataBase Connected');
+// }).catch((error)=>{console.log(error)});
 
 
-myapp.get("/",(req,res)=>{
-    res.json({message: "Request Accepted"})
+// myapp.get("/",(req,res)=>{
+//     res.json({message: "Request Accepted"})
+// })
+// myapp.listen(4000,()=>{console.log("server up and Running")})
+
+// export {myapp};
+
+import express from 'express';
+
+const Tiger = express();
+
+Tiger.get('/',(req,res)=>{
+    res.json("please work")
 })
-myapp.listen(4000,()=>{console.log("server up and Running")})
 
-export {myapp};
+Tiger.listen(4000,()=>{console.log("worked")});
+
+export {Tiger};
