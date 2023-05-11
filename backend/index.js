69c7691e274936686101fecd6f3ff8c789ceaaf1
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-11 15:17:03 +0530

    adsssssdfsfe

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 import express from "express";
-import cors from 'cors';
-import mongoose from "mongoose";
-import vendor from './routes/Vendor.js';
+// import cors from 'cors';
+// import mongoose from "mongoose";
+// import vendor from './routes/Vendor.js';
 
 const myapp = express();
 myapp.use(express.json());
-myapp.use(cors());
-myapp.use('/api/v1/vendors', vendor )
+// myapp.use(cors());
+// myapp.use('/api/v1/vendors', vendor )
 
-await mongoose.connect(process.env.MongoURI).then(()=>{
-    console.log('DataBase Connected');
-}).catch((error)=>{console.log(error)});
+// await mongoose.connect(process.env.MongoURI).then(()=>{
+//     console.log('DataBase Connected');
+// }).catch((error)=>{console.log(error)});
 
 
 myapp.get("/",(req,res)=>{
--- backend/routes/Vendor.js
+++ backend/routes/Vendor.js
@@ -2,9 +2,6 @@ import { Router } from "express";
 import {signIn,signUp} from '../Controllers/authControllers.js'
 
 const vendor = Router();
-
-
-
 vendor.post("/signUp", signUp);
 vendor.post("/signIn", signIn);
 
