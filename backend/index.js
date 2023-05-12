a5d868d8f0e25bf5738a9e40c0829690e285b9c5
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-12 09:42:13 +0530

    1

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import vendor from './routes/Vendor.js';
+import vendor from "./routes/Vendor.js";
 
 const myapp = express();
 myapp.use(express.json());
 myapp.use(cors());
-myapp.use('/api/v1/vendors', vendor )
+myapp.use("/api/v1/vendors", vendor);
 
-const connectDB = async()=>{
-  await mongoose.connect(process.env.MongoURI).then(() => {
-    console.log("DataBase Connected");
-}).catch((error) => {
-    console.log(error);
-});
-}
-
-
-connectDB();
-
-
+const connectDB = () => {
+  return new Promise((res,rej)=>{
+    mongoose
+    .connect("mongodb+srv://dudejagarvit3:Garvit1839@cluster0.aa2bbme.mongodb.net/")
+    .then(() => {
+      console.log("DataBase Connected");
+      res("working fine");
+    })
+    .catch((error) => {
+      console.log(error);
+    })})
+    
+};
+await connectDB();
 myapp.get("/", (req, res) => {
   res.json({ message: "Request Accepted" });
 });
