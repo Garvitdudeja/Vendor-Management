43ac8cd0ff66ab4c05a07f94655d35e273c17fc6
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-15 15:18:34 +0530

    bhagwan ji chla do 7

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ import vendor from "./routes/Vendor.js";
 import user from "./routes/Users.js";
 
 const myapp = express();
-myapp.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+myapp.use(cors({ credentials: true, origin: "*" }));
 myapp.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
