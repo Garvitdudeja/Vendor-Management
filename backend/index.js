3e414c639cd9392cf20bf512955ae6f64a35b53a
GarvitDudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-05-15 17:11:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ import user from "./routes/Users.js";
 
 const myapp = express();
 myapp.use(cors(corsOptions));
-myapp.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// myapp.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 myapp.use(express.json());
 myapp.use(cookieParser());
